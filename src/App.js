d57e2bbcc2fcdf4906bd667e75d9cd7365beb3d6
Pablo <pablo.compagni@bluealba.com>
2023-01-31 14:55:06 -0300

    another change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>change 1</div>
+        <div>change 2</div>
         <p>
           Edit <code>src/App.js</code> and save to reload. Hola!
         </p>
