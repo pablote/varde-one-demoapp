b229a5a55c3bdcc3e7c4d53f81461878ef080c86
Pablo <pablo.compagni@bluealba.com>
2023-02-10 18:09:06 -0300

    change 8

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <div>change 5</div>
         <div>change 6</div>
         <div>change 7</div>
+        <div>change 8</div>
         <p>
           Edit <code>src/App.js</code> and save to reload. Hola!
         </p>
